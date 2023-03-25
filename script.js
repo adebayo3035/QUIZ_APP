faae757005be1b30a4571d7bcace9744529e9171
abdul-rahmon <abdulrahmonadebayo@gmail.com>
2023-03-25 11:52:38 +0100

    Created startquiz and showQuiz function

--- script.js
+++ script.js
@@ -2,40 +2,68 @@ const questions = [
     {
         question: "Which is the largest animal in the world?",
         answers: [
-            {text: "Shark", correct: "false"},
-            {text: "Blue Whale", correct: "true"},
-            {text: "Elephant", correct: "false"},
-            {text: "Giraffe", correct: "false"},
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
         ]
     },
 
     {
         question: "Which is the smallest country in the world?",
         answers: [
-            {text: "Vatican City", correct: "true"},
-            {text: "Bhutan", correct: "true"},
-            {text: "Nepal", correct: "false"},
-            {text: "Shri Lanka", correct: "false"},
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Shri Lanka", correct: false},
         ]
     },
 
     {
         question: "Which is the largest desert in the world?",
         answers: [
-            {text: "Kalahari", correct: "false"},
-            {text: "Gobi", correct: "false"},
-            {text: "Sahara", correct: "false"},
-            {text: "Antarctica", correct: "true"},
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
         ]
     },
 
     {
         question: "Which is the smallest continent in the world?",
         answers: [
-            {text: "Asia", correct: "false"},
-            {text: "Australia", correct: "true"},
-            {text: "Arctic", correct: "false"},
-            {text: "Africa", correct: "false"},
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
         ]
     },
-]
+];
+
+const questionElement = document.getElementById("question")
+const answerButton = document.getElementById("answer-buttons")
+const nextButton = document.getElementById("next-btn")
+
+//calculate sccore
+let currentQuestionIndex = 0;
+let score = 0;
+
+const startQuiz = () =>{
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.textContent ="Next";
+    showQuestion();
+}
+const showQuestion = () =>{
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNumber = currentQuestionIndex + 1;
+    questionElement.textContent = `${questionNumber}.  ${currentQuestion.question}`
+
+    //Display options
+    currentQuestion.answers.forEach(answer =>{
+        const button = document.createElement("button")
+        button.textContent = answer.text;
+        button.classList.add('btn')
+        answerButton.appendChild(button)
+    })
+}
